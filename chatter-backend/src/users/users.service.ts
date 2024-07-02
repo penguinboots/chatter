ce9d87143339c1cc041aa990f3719064a46d69fb
Sabrina Wang <sabrina.wang@applydigital.com>
2024-07-02 10:33:44 -0700

    create entities

--- chatter-backend/src/users/users.service.ts
+++ chatter-backend/src/users/users.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly usersReponsitory: UsersRepository) {}
+
   create(createUserInput: CreateUserInput) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    return this.usersReponsitory.find({});
   }
 
   findOne(id: number) {
