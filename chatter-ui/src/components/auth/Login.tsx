382ffe15bc357d2a74ba73872d97b1a51e500002
Sabrina Wang <sabrina.wang@applydigital.com>
2024-07-03 11:35:16 -0700

    add login

--- chatter-ui/src/components/auth/Login.tsx
+++ chatter-ui/src/components/auth/Login.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
+import { useLogin } from "../../hooks/useLogin";
 
 export function Login() {
+  const { login, error } = useLogin();
+
   return (
-    <Auth submitLabel="Login" onSubmit={async () => {}}>
+    <Auth submitLabel="Login" onSubmit={(request) => login(request)}>
       <Link to="/signup" style={{ alignSelf: "center" }}>
         <MUILink>Sign Up</MUILink>
       </Link>
