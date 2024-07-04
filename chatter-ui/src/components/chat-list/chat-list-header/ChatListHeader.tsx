f872861b2bc86a3e44e060444080cbdb42d7f61b
Sabrina Wang <sabrina.wang@applydigital.com>
2024-07-04 12:49:19 -0700

    add chat modal

--- chatter-ui/src/components/chat-list/chat-list-header/ChatListHeader.tsx
+++ chatter-ui/src/components/chat-list/chat-list-header/ChatListHeader.tsx
@@ -1,11 +1,15 @@
 import AddCircle from "@mui/icons-material/AddCircle";
 import { AppBar, IconButton, Toolbar } from "@mui/material";
 
-export function ChatListHeader() {
+interface ChatListHeaderProps {
+  handleAddChat: () => void;
+}
+
+export function ChatListHeader({ handleAddChat }: ChatListHeaderProps) {
   return (
     <AppBar position="static" color="transparent">
       <Toolbar>
-        <IconButton size="large" edge="start">
+        <IconButton size="large" edge="start" onClick={handleAddChat}>
           <AddCircle />
         </IconButton>
       </Toolbar>
